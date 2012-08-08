56d55c5b2174e01934cb04ef3e6adfbd7b7d2e6d
Alan LaMielle <alamielle@vmware.com>
2012-08-08 08:51:59 -0700

    Removes unused callbacks and adds focus tab call

--- extension/background.js
+++ extension/background.js
@@ -1,39 +1,33 @@
 var filters =
   {
-    urls: ["https://www.google.com/*"]
+    urls: ["https://www.google.com/*",
+           "https://www.ackerleyplumbing.com/",
+           "https://10.138.102.115/*"]
   };
 
 var onBeforeRequest = function(details) {
-    console.log("onBeforeRequest: " + details.url);
-  };
-var onBeforeSendHeaders = function(details) {
-    console.log("onBeforeSendHeaders: " + details.url);
-  };
+   console.log("onBeforeRequest: " + details.tabId + " " + details.url);
+};
 var onSendHeaders = function(details) {
-    console.log("onSendHeaders: " + details.url);
-  };
+   console.log("onSendHeaders: " + details.tabId + " " + details.url);
+   setTimeout(function() {
+      console.log("Setting tabId " + details.tabId + " to be active")
+      chrome.tabs.update(details.tabId, {active: true});
+   },
+   3000);
+};
 var onHeadersReceived = function(details) {
-    console.log("onHeadersReceived: " + details.url);
-  };
-var onBeforeRedirect = function(details) {
-    console.log("onBeforeRedirect: " + details.url);
-  };
-var onResponseStarted = function(details) {
-    console.log("onResponseStarted: " + details.url);
-  };
+   console.log("onHeadersReceived: " + details.url);
+};
 var onCompleted = function(details) {
-    console.log("onCompleted: " + details.url);
-  };
+   console.log("onCompleted: " + details.url);
+};
 var onErrorOccurred = function(details) {
-    console.log("onErrorOccurred: " + details.url);
-  };
+   console.log("onErrorOccurred: " + details.url);
+};
 
 chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest,filters);
-chrome.webRequest.onBeforeSendHeaders.addListener(onBeforeSendHeaders,filters);
 chrome.webRequest.onSendHeaders.addListener(onSendHeaders,filters);
 chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived,filters);
-chrome.webRequest.onBeforeRedirect.addListener(onBeforeRedirect,filters);
-chrome.webRequest.onResponseStarted.addListener(onResponseStarted,filters);
 chrome.webRequest.onCompleted.addListener(onCompleted,filters);
 chrome.webRequest.onErrorOccurred.addListener(onErrorOccurred,filters);
-
